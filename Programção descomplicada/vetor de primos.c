6da976586c7d12399f7898c2027cde18675a2778
edmarjd <fedmar049@gmail.com>
2025-05-06 03:42:14 +0000

    vetor de primos

---
+++ Programção descomplicada/vetor de primos.c
@@ -0,0 +1,40 @@
+/* Leia 10 n´umeros inteiros e armazene em um vetor. Em seguida escreva os elementos
+ que s˜ ao primos e suas respectivas posic ¸˜ oes no vetor
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool par(int nums);
+
+int main(){
+
+    int valor[10];
+    printf("Digite os valores de cada indice do vetor:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%d", &valor[i]);
+	}
+	
+	for (int i = 0; i < 10; i++){
+        if (par(valor[i])) {
+            printf("Numero primo %d , posicao %d\n", valor[i], i);
+        } else {
+            continue;
+        }
+    }
+    return 0;
+}
+bool par(int nums){
+    if (nums <= 1) return false;
+    if (nums == 2) return true;   
+    if (nums %2 == 0)return false;
+        
+    int ponto = sqrt(nums);
+    for(int  i = 3; i <= ponto; i+= 2){
+		if (nums % i == 0) {
+			return false;
+		}
+    }
+    return true;
+}
