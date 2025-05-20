628343362f596fec9a12fefef894e8b6f89882ca
edmarjd <fedmar049@gmail.com>
2025-05-20 19:50:38 -0300

    A média do vetor, e o desvio padrão.

---
+++ Programação descomplicada/Vetores/vetor3.c
@@ -0,0 +1,29 @@
+/*Fac¸a um programa que calcule o desvio padr ˜ao de um vetor v contendo n = 10 n ´umeros,
+onde m ´e a media do vetor.*/
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    float v[10];
+    float m, soma = 0, resultado = 0, Desv;
+    printf("Preencha o vetor:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%f", &v[i]);
+        soma += v[i];
+    }
+
+    m = soma/10;
+
+    for (int i = 0; i < 10; i++) {
+       resultado += pow(v[i] - m, 2);
+    }
+
+    Desv = sqrt(resultado / (10 - 1));
+
+    printf("A media do vetor e : %.2f", m);
+    printf("\nO desvio padrao e: %.2f", Desv);
+
+    return 0;
+}
